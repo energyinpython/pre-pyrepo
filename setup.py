92e44594e12d1110247f011e51734e5ce1fe0b8e
energyinpython <rubinn2@wp.pl>
2022-03-26 14:28:34 +0100

    v1

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrepo-aleksandraba",
-    version="0.0.4",
+    version="0.0.6",
     author="Aleksandra Ba",
     author_email="aleksandra.baczkiewicz@phd.usz.edu.pl",
     description="Package with Methods for Multi-Criteria Decision Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/energyinpython/pre-pyrepo",
-    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
